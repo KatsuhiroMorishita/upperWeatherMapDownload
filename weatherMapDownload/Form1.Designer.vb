d04214de5d95e883212c5d9fd14d265d2eb55840
morishita <morimori.ynct@gmail.com>
2017-10-06 13:49:51 +0900

    初めてのコミット

---
+++ weatherMapDownload/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.clockCheckTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ICCondition = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ErrorInfo = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.NextDwload = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PicBox700_00 = New System.Windows.Forms.PictureBox()
+        Me.PicBox700_12 = New System.Windows.Forms.PictureBox()
+        Me.PicBox850_00 = New System.Windows.Forms.PictureBox()
+        Me.PicBox850_12 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PicBox500_00 = New System.Windows.Forms.PictureBox()
+        Me.PicBox500_12 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.StatusStrip.SuspendLayout()
+        CType(Me.PicBox700_00, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox700_12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox850_00, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox850_12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox500_00, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox500_12, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'clockCheckTimer
+        '
+        Me.clockCheckTimer.Enabled = True
+        Me.clockCheckTimer.Interval = 10000
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ICCondition, Me.ErrorInfo, Me.NextDwload})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 541)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(915, 23)
+        Me.StatusStrip.TabIndex = 0
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'ICCondition
+        '
+        Me.ICCondition.ForeColor = System.Drawing.Color.DarkGreen
+        Me.ICCondition.Name = "ICCondition"
+        Me.ICCondition.Size = New System.Drawing.Size(182, 18)
+        Me.ICCondition.Text = "Internet Connection Condition"
+        '
+        'ErrorInfo
+        '
+        Me.ErrorInfo.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ErrorInfo.Name = "ErrorInfo"
+        Me.ErrorInfo.Size = New System.Drawing.Size(66, 18)
+        Me.ErrorInfo.Text = "Error info."
+        '
+        'NextDwload
+        '
+        Me.NextDwload.Name = "NextDwload"
+        Me.NextDwload.Size = New System.Drawing.Size(103, 18)
+        Me.NextDwload.Text = "Download clock:"
+        '
+        'PicBox700_00
+        '
+        Me.PicBox700_00.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox700_00.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox700_00.Location = New System.Drawing.Point(308, 12)
+        Me.PicBox700_00.Name = "PicBox700_00"
+        Me.PicBox700_00.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox700_00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox700_00.TabIndex = 1
+        Me.PicBox700_00.TabStop = False
+        '
+        'PicBox700_12
+        '
+        Me.PicBox700_12.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox700_12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox700_12.Location = New System.Drawing.Point(308, 271)
+        Me.PicBox700_12.Name = "PicBox700_12"
+        Me.PicBox700_12.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox700_12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox700_12.TabIndex = 2
+        Me.PicBox700_12.TabStop = False
+        '
+        'PicBox850_00
+        '
+        Me.PicBox850_00.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox850_00.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox850_00.Location = New System.Drawing.Point(612, 12)
+        Me.PicBox850_00.Name = "PicBox850_00"
+        Me.PicBox850_00.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox850_00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox850_00.TabIndex = 3
+        Me.PicBox850_00.TabStop = False
+        '
+        'PicBox850_12
+        '
+        Me.PicBox850_12.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox850_12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox850_12.Location = New System.Drawing.Point(612, 271)
+        Me.PicBox850_12.Name = "PicBox850_12"
+        Me.PicBox850_12.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox850_12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox850_12.TabIndex = 4
+        Me.PicBox850_12.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(396, 255)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Fig. 700 hPa 00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(396, 514)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Fig. 700 hPa 12"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(728, 514)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 12)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Fig. 850 hPa 12"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(728, 255)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Fig. 850 hPa 00"
+        '
+        'PicBox500_00
+        '
+        Me.PicBox500_00.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox500_00.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox500_00.Location = New System.Drawing.Point(12, 12)
+        Me.PicBox500_00.Name = "PicBox500_00"
+        Me.PicBox500_00.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox500_00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox500_00.TabIndex = 9
+        Me.PicBox500_00.TabStop = False
+        '
+        'PicBox500_12
+        '
+        Me.PicBox500_12.BackgroundImage = Global.upperWeatherMapDownload.My.Resources.Resources.no_image2
+        Me.PicBox500_12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PicBox500_12.Location = New System.Drawing.Point(12, 271)
+        Me.PicBox500_12.Name = "PicBox500_12"
+        Me.PicBox500_12.Size = New System.Drawing.Size(290, 240)
+        Me.PicBox500_12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox500_12.TabIndex = 10
+        Me.PicBox500_12.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(105, 514)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 12)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Fig. 500 hPa 12"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(105, 256)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(84, 12)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Fig. 500 hPa 00"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(915, 564)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PicBox500_12)
+        Me.Controls.Add(Me.PicBox500_00)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PicBox850_12)
+        Me.Controls.Add(Me.PicBox850_00)
+        Me.Controls.Add(Me.PicBox700_12)
+        Me.Controls.Add(Me.PicBox700_00)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Upper Weather Map Downloader"
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        CType(Me.PicBox700_00, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox700_12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox850_00, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox850_12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox500_00, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox500_12, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents clockCheckTimer As System.Windows.Forms.Timer
+    Friend WithEvents StatusStrip As System.Windows.Forms.StatusStrip
+    Friend WithEvents ICCondition As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ErrorInfo As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents NextDwload As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents PicBox700_00 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicBox700_12 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicBox850_00 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicBox850_12 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PicBox500_00 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicBox500_12 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ weatherMapDownload/Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class Form1
+    ''' <summary>
+    ''' タイマーが起動するごとに、気象庁のHPを確認して高層天気図をダウンロードする
+    ''' 高層天気図はUTCで0時と12時の予報が出されている。
+    ''' 2011/9/13時点では公開タイミングは分からん。
+    ''' 
+    ''' [開発履歴]
+    ''' 2011/9/13 普通の天気図をダウンロードするソフトウェアを基に高層天気図をダウンロードするプログラムを製作開始
+    '''           今回は一つのウィンドウの中に4つのピクチャーボックスを並べたが、これ以上に拡張するならマルチウィンドウにすべきだ。
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub clockCheckTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clockCheckTimer.Tick
+        Dim t As DateTime = Date.UtcNow                                         ' 高層天気図はUTC時刻を基に発表される
+        Dim t2 As DateTime
+        Dim wc As New System.Net.WebClient()
+        Dim path As String
+        Dim yy, mm, dd, hh As String
+        Dim fname, savePath As String
+        Dim hour As Integer
+        Const releaseTime As Double = 48600.0                                   ' ある時刻分の天気図が公開されるまでの時間差（とりあえず、13時間30分後としてみる）
+
+        If System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() Then  ' ネットに接続しているかを確認
+            Me.ICCondition.Text = "Internet Connection Condition: Connect"      ' 接続できていることを示す。
+            ' ダウンロードファイル名生成の準備
+            t = t.AddSeconds(-releaseTime)                                      ' 既にリリースされている分がほしいので、リリースまでのディレイを引いて計算する
+            hour = Math.Floor(t.Hour / 12) * 12
+            yy = (t.Year Mod 100).ToString("d2")                                ' 時分秒を指定のフォーマットで文字列化する
+            mm = (t.Month).ToString("d2")
+            dd = t.Day.ToString("d2")
+            hh = hour.ToString("d2")
+            ' まずは500hPa
+            fname = yy + mm + dd + "upper500hPa_" + hh + ".png"                 ' HDへ保存するときのファイル名を作る
+            savePath = System.IO.Directory.GetCurrentDirectory() + "\" + fname
+            t2 = New DateTime(t.Year, t.Month, t.Day, hour, 0, 0).AddSeconds(releaseTime)           ' ダウンロードしたいファイルのリリース時刻を少し余裕をもって計算する
+            If System.IO.File.Exists(savePath) = False Then                                         ' ファイルが未ダウンロードならダウンロードする
+                If t2 < Date.UtcNow Then                                                            ' 2時間10分経過していればダウンロードを実行する
+                    Try
+                        path = "http://www.jma.go.jp/jmh/wmapimgs/lrg_" + hh + "_auas50.png"
+                        wc.DownloadFile(path, savePath)                                             ' ダウンロード
+                        If hour = 0 Then Me.PicBox500_00.Image = System.Drawing.Image.FromFile(savePath) ' 読み込めたら、表示する
+                        If hour = 12 Then Me.PicBox500_12.Image = System.Drawing.Image.FromFile(savePath)
+                        Me.ErrorInfo.Text = "ダウンロードしました。"
+                        t2 = t2.AddHours(12)                                                        ' 次のダウンロード時刻を計算する
+                    Catch ex As Exception
+                        Me.ErrorInfo.Text = "何らかのエラーが発生しました。orz"
+                    End Try
+                End If
+            Else
+                t2 = t2.AddHours(12)                                                                ' 既にダウンロード済みであった場合、次のダウンロード時刻を計算する
+            End If
+            ' 次は700hPa
+            fname = yy + mm + dd + "upper700hPa_" + hh + ".png"                 ' HDへ保存するときのファイル名を作る
+            savePath = System.IO.Directory.GetCurrentDirectory() + "\" + fname
+            t2 = New DateTime(t.Year, t.Month, t.Day, hour, 0, 0).AddSeconds(releaseTime)           ' ダウンロードしたいファイルのリリース時刻を少し余裕をもって計算する
+            If System.IO.File.Exists(savePath) = False Then                                         ' ファイルが未ダウンロードならダウンロードする
+                If t2 < Date.UtcNow Then                                                            ' 2時間10分経過していればダウンロードを実行する
+                    Try
+                        path = "http://www.jma.go.jp/jmh/wmapimgs/lrg_" + hh + "_auas70.png"
+                        wc.DownloadFile(path, savePath)                                             ' ダウンロード
+                        If hour = 0 Then Me.PicBox700_00.Image = System.Drawing.Image.FromFile(savePath) ' 読み込めたら、表示する
+                        If hour = 12 Then Me.PicBox700_12.Image = System.Drawing.Image.FromFile(savePath)
+                        Me.ErrorInfo.Text = "ダウンロードしました。"
+                        t2 = t2.AddHours(12)                                                        ' 次のダウンロード時刻を計算する
+                    Catch ex As Exception
+                        Me.ErrorInfo.Text = "何らかのエラーが発生しました。orz"
+                    End Try
+                End If
+            Else
+                t2 = t2.AddHours(12)                                                                ' 既にダウンロード済みであった場合、次のダウンロード時刻を計算する
+            End If
+            ' 最後は850hPa
+            fname = yy + mm + dd + "upper850hPa_" + hh + ".png"                                     ' HDへ保存するときのファイル名を作る
+            savePath = System.IO.Directory.GetCurrentDirectory() + "\" + fname
+            t2 = New DateTime(t.Year, t.Month, t.Day, hour, 0, 0).AddSeconds(releaseTime)           ' ダウンロードしたいファイルのリリース時刻を少し余裕をもって計算する
+            If System.IO.File.Exists(savePath) = False Then                                         ' ファイルが未ダウンロードならダウンロードする
+                If t2 < Date.UtcNow Then                                                            ' 2時間10分経過していればダウンロードを実行する
+                    Try
+                        path = "http://www.jma.go.jp/jmh/wmapimgs/lrg_" + hh + "_auas85.png"
+                        wc.DownloadFile(path, savePath)                                             ' ダウンロード
+                        If hour = 0 Then Me.PicBox850_00.Image = System.Drawing.Image.FromFile(savePath) ' 読み込めたら、表示する
+                        If hour = 12 Then Me.PicBox850_12.Image = System.Drawing.Image.FromFile(savePath)
+                        Me.ErrorInfo.Text = "ダウンロードしました。"
+                        t2 = t2.AddHours(12)                                                        ' 次のダウンロード時刻を計算する
+                    Catch ex As Exception
+                        Me.ErrorInfo.Text = "何らかのエラーが発生しました。orz"
+                    End Try
+                End If
+            Else
+                t2 = t2.AddHours(12)                                                                ' 既にダウンロード済みであった場合、次のダウンロード時刻を計算する
+            End If
+            Me.NextDwload.Text = "Next Download clock(UTC):" + t2.ToString                          ' ダウンロード時刻を表示する
+            Me.NextDwload.Text += "   Last:" + CInt(Date.UtcNow.Subtract(t2).TotalSeconds).ToString() + "[s]"
+        Else
+            Me.ICCondition.Text = "Internet Connection Condition: Disconnect"
+        End If
+        wc.Dispose()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() Then  ' ネットに接続しているかを確認
+            Me.ICCondition.Text = "Internet Connection Condition: Connect"
+        Else
+            Me.ICCondition.Text = "Internet Connection Condition: Disconnect"
+        End If
+    End Sub
+End Class
